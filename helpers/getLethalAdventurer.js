164d5384d4e54fd7cd8e7ef2c77bc76e1e56691a
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-23 15:05:03 -0300

    função getLethalAdventurer implementada

--- helpers/getLethalAdventurer.js
+++ helpers/getLethalAdventurer.js
@@ -1,5 +1,30 @@
-const getLethalAdventurer = () => {
-  // Desenvolva seu código dentro dessa função...
+function calculatePower(weaponsArr, character) {
+  let totalDamage = 0;
+
+  const { weapons } = character;
+
+  weapons.forEach((weapon) => {
+    totalDamage += weaponsArr.find(({ id }) => weapon === id).damage;
+  });
+
+  return totalDamage;
+}
+
+const getLethalAdventurer = (data) => {
+  const { adventurers, weapons } = data;
+
+  const mostLethal = adventurers.reduce((acc, adventurer) => {
+    const actual = calculatePower(weapons, acc);
+    const compare = calculatePower(weapons, adventurer);
+
+    acc = compare > actual
+      ? adventurer
+      : acc;
+
+    return acc;
+  }, { weapons: [] });
+
+  return mostLethal;
 };
 
 export default getLethalAdventurer;
