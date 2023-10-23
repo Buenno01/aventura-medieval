19a697faf7efc262dedc4dd8fd9e1d16ae27d9dc
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-23 15:17:22 -0300

    função getMostFearedMonster implementada

--- helpers/getMostFearedMonster.js
+++ helpers/getMostFearedMonster.js
@@ -1,5 +1,18 @@
+function getTotalDamage(monster) {
+  const { special_abilities } = monster;
+  const totalDamage = special_abilities.reduce((acc, { damage }) => acc += damage, 0);
+
+  return totalDamage;
+}
+
 const getMostFearedMonster = (data) => {
-  // Desenvolva seu código dentro dessa função...
+  const mostFeared = data.monsters.reduce((acc, monster) => {
+    return (acc = getTotalDamage(monster) > getTotalDamage(acc)
+      ? monster
+      : acc);
+  }, { special_abilities: [] });
+
+  return mostFeared;
 };
 
 export default getMostFearedMonster;
