e4386135c497aeba59f6ad620e7f0949595bc51b
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-23 15:21:54 -0300

    função sorting by property implementada

--- helpers/sortingByProperty.js
+++ helpers/sortingByProperty.js
@@ -1,5 +1,12 @@
 const sortingByProperty = (what, p1, p2) => {
-  // Desenvolva seu código dentro dessa função...
+  const sorted = what.sort((a, b) => {
+    if (p2) {
+      return b[p1][p2] - a[p1][p2];
+    }
+    return b[p1] - a[p1];
+  });
+
+  return sorted;
 };
 
 export default sortingByProperty;
