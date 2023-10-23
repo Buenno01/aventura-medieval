7c9a02e1ba96951ae1189beee69271eb4bb4a875
Vinicius Bueno Costa <buenov535@gmail.com>
2023-10-23 10:20:47 -0300

    Função findCharacter implementada

--- helpers/findCharacter.js
+++ helpers/findCharacter.js
@@ -1,5 +1,17 @@
-const findCharacter = () => {
-  // aaa
+const findCharacter = (data, characterId) => {
+  const charCategories = Object.keys(data);
+  let character;
+
+  charCategories.forEach((category) => {
+    const foundCharacter = data[category].find((char) => char.id == characterId);
+
+    character = foundCharacter 
+      ? foundCharacter
+      : character;
+  });
+
+  if (!character) throw new Error('Character not found.');
+  return character;
 };
 
 export default findCharacter;
